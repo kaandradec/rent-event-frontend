32611d4a9e9b9f92cad23e9842c782e19d74cb8e
kaandradec <130163890+kaandradec@users.noreply.github.com>
2024-06-12 01:29:52 -0500

    feat: dashboard Services

--- src/pages/user/Dashboard.tsx
+++ src/pages/user/Dashboard.tsx
@@ -2,6 +2,20 @@ import { Button } from "@/components/ui/button"
 import { DashboardMenu } from "@/components/DashboardMenu"
 import { DashboardHeader } from "@/components/DashboardHeader";
 import { useState } from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { DropdownMenu, DropdownMenuCheckboxItem, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+
+
+import {
+  File,
+  ListFilter,
+  MoreHorizontal,
+  PlusCircle,
+} from "lucide-react"
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { Image } from "@nextui-org/react";
+import { Badge } from "@/components/ui/badge";
 
 export default function Dashboard() {
 
@@ -47,23 +61,367 @@ const DashboardOrders = () => (<>
 </>)
 
 const DashboardServices = () => (
-  <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
-    <div className="flex items-center">
-      <h1 className="text-lg font-semibold md:text-2xl">Inventario</h1>
-    </div>
-    <div
-      className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm" x-chunk="dashboard-02-chunk-1"
-    >
-      <div className="flex flex-col items-center gap-1 text-center">
-        <h3 className="text-2xl font-bold tracking-tight">
-          No existen servicios
-        </h3>
-        <p className="text-sm text-muted-foreground">
-          Puedes agregar un nuevo servicio
-        </p>
-        <Button className="mt-4">Añadir servicio</Button>
+  <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 md:py-6">
+    <Tabs defaultValue="all">
+      <div className="flex items-center">
+        <TabsList>
+          <TabsTrigger value="all">Todos</TabsTrigger>
+          <TabsTrigger value="active">Utilitario</TabsTrigger>
+          <TabsTrigger value="draft">Humano</TabsTrigger>
+          <TabsTrigger value="archived" className="hidden sm:flex">
+            Transporte
+          </TabsTrigger>
+        </TabsList>
+        <div className="ml-auto flex items-center gap-2">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline" size="sm" className="h-8 gap-1">
+                <ListFilter className="h-3.5 w-3.5" />
+                <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
+                  Filtro
+                </span>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>Filtrar por</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuCheckboxItem checked>
+                Utilitario
+              </DropdownMenuCheckboxItem>
+              <DropdownMenuCheckboxItem>Humano</DropdownMenuCheckboxItem>
+              <DropdownMenuCheckboxItem>
+                Transporte
+              </DropdownMenuCheckboxItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+          <Button size="sm" variant="outline" className="h-8 gap-1">
+            <File className="h-3.5 w-3.5" />
+            <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
+              Exportar
+            </span>
+          </Button>
+          <Button size="sm" className="h-8 gap-1">
+            <PlusCircle className="h-3.5 w-3.5" />
+            <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
+              Añadir servicio
+            </span>
+          </Button>
+        </div>
       </div>
-    </div>
+      <TabsContent value="all">
+        <Card x-chunk="dashboard-06-chunk-0">
+          <CardHeader>
+            <CardTitle>Servicios</CardTitle>
+            <CardDescription>
+              Administrar servicios que se ofrecen en los eventos
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="hidden w-[100px] sm:table-cell">
+                    <span className="sr-only">Imagen</span>
+                  </TableHead>
+                  <TableHead>Nombre</TableHead>
+                  <TableHead>Estado</TableHead>
+                  <TableHead className="hidden md:table-cell">
+                    Precio
+                  </TableHead>
+                  <TableHead className="hidden md:table-cell">
+                    Stock Total
+                  </TableHead>
+                  <TableHead className="hidden md:table-cell">
+                    Modificado el
+                  </TableHead>
+                  <TableHead>
+                    <span className="sr-only">Acciones</span>
+                  </TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                <TableRow>
+                  <TableCell className="hidden sm:table-cell">
+                    <Image
+                      alt="Product image"
+                      className="aspect-square rounded-md object-cover"
+                      height="64"
+                      src="https://ui.shadcn.com/placeholder.svg"
+                      width="64"
+                    />
+                  </TableCell>
+                  <TableCell className="font-medium">
+                    Laser Lemonade Machine
+                  </TableCell>
+                  <TableCell>
+                    <Badge variant="default">Activo</Badge>
+                  </TableCell>
+                  <TableCell className="hidden md:table-cell">
+                    $499.99
+                  </TableCell>
+                  <TableCell className="hidden md:table-cell">
+                    25
+                  </TableCell>
+                  <TableCell className="hidden md:table-cell">
+                    2023-07-12 10:42 AM
+                  </TableCell>
+                  <TableCell>
+                    <DropdownMenu>
+                      <DropdownMenuTrigger asChild>
+                        <Button
+                          aria-haspopup="true"
+                          size="icon"
+                          variant="ghost"
+                        >
+                          <MoreHorizontal className="h-4 w-4" />
+                          <span className="sr-only">Toggle menu</span>
+                        </Button>
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent align="end">
+                        <DropdownMenuLabel>Acciones</DropdownMenuLabel>
+                        <DropdownMenuItem>Editar</DropdownMenuItem>
+                        <DropdownMenuItem>Eliminar/Desabilitar</DropdownMenuItem>
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell className="hidden sm:table-cell">
+                    <Image
+                      alt="Product image"
+                      className="aspect-square rounded-md object-cover"
+                      height="64"
+                      src="https://ui.shadcn.com/placeholder.svg"
+                      width="64"
+                    />
+                  </TableCell>
+                  <TableCell className="font-medium">
+                    Hypernova Headphones
+                  </TableCell>
+                  <TableCell>
+                    <Badge variant="destructive">Inactivo</Badge>
+                  </TableCell>
+                  <TableCell className="hidden md:table-cell">
+                    $129.99
+                  </TableCell>
+                  <TableCell className="hidden md:table-cell">
+                    100
+                  </TableCell>
+                  <TableCell className="hidden md:table-cell">
+                    2023-10-18 03:21 PM
+                  </TableCell>
+                  <TableCell>
+                    <DropdownMenu>
+                      <DropdownMenuTrigger asChild>
+                        <Button
+                          aria-haspopup="true"
+                          size="icon"
+                          variant="ghost"
+                        >
+                          <MoreHorizontal className="h-4 w-4" />
+                          <span className="sr-only">Toggle menu</span>
+                        </Button>
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent align="end">
+                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                        <DropdownMenuItem>Edit</DropdownMenuItem>
+                        <DropdownMenuItem>Delete</DropdownMenuItem>
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell className="hidden sm:table-cell">
+                    <Image
+                      alt="Product image"
+                      className="aspect-square rounded-md object-cover"
+                      height="64"
+                      src="https://ui.shadcn.com/placeholder.svg"
+                      width="64"
+                    />
+                  </TableCell>
+                  <TableCell className="font-medium">
+                    AeroGlow Desk Lamp
+                  </TableCell>
+                  <TableCell>
+                    <Badge variant="outline">Active</Badge>
+                  </TableCell>
+                  <TableCell className="hidden md:table-cell">
+                    $39.99
+                  </TableCell>
+                  <TableCell className="hidden md:table-cell">
+                    50
+                  </TableCell>
+                  <TableCell className="hidden md:table-cell">
+                    2023-11-29 08:15 AM
+                  </TableCell>
+                  <TableCell>
+                    <DropdownMenu>
+                      <DropdownMenuTrigger asChild>
+                        <Button
+                          aria-haspopup="true"
+                          size="icon"
+                          variant="ghost"
+                        >
+                          <MoreHorizontal className="h-4 w-4" />
+                          <span className="sr-only">Toggle menu</span>
+                        </Button>
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent align="end">
+                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                        <DropdownMenuItem>Edit</DropdownMenuItem>
+                        <DropdownMenuItem>Delete</DropdownMenuItem>
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell className="hidden sm:table-cell">
+                    <Image
+                      alt="Product image"
+                      className="aspect-square rounded-md object-cover"
+                      height="64"
+                      src="https://ui.shadcn.com/placeholder.svg"
+                      width="64"
+                    />
+                  </TableCell>
+                  <TableCell className="font-medium">
+                    TechTonic Energy Drink
+                  </TableCell>
+                  <TableCell>
+                    <Badge variant="secondary">Draft</Badge>
+                  </TableCell>
+                  <TableCell className="hidden md:table-cell">
+                    $2.99
+                  </TableCell>
+                  <TableCell className="hidden md:table-cell">
+                    0
+                  </TableCell>
+                  <TableCell className="hidden md:table-cell">
+                    2023-12-25 11:59 PM
+                  </TableCell>
+                  <TableCell>
+                    <DropdownMenu>
+                      <DropdownMenuTrigger asChild>
+                        <Button
+                          aria-haspopup="true"
+                          size="icon"
+                          variant="ghost"
+                        >
+                          <MoreHorizontal className="h-4 w-4" />
+                          <span className="sr-only">Toggle menu</span>
+                        </Button>
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent align="end">
+                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                        <DropdownMenuItem>Edit</DropdownMenuItem>
+                        <DropdownMenuItem>Delete</DropdownMenuItem>
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell className="hidden sm:table-cell">
+                    <Image
+                      alt="Product image"
+                      className="aspect-square rounded-md object-cover"
+                      height="64"
+                      src="https://ui.shadcn.com/placeholder.svg"
+                      width="64"
+                    />
+                  </TableCell>
+                  <TableCell className="font-medium">
+                    Gamer Gear Pro Controller
+                  </TableCell>
+                  <TableCell>
+                    <Badge variant="outline">Active</Badge>
+                  </TableCell>
+                  <TableCell className="hidden md:table-cell">
+                    $59.99
+                  </TableCell>
+                  <TableCell className="hidden md:table-cell">
+                    75
+                  </TableCell>
+                  <TableCell className="hidden md:table-cell">
+                    2024-01-01 12:00 AM
+                  </TableCell>
+                  <TableCell>
+                    <DropdownMenu>
+                      <DropdownMenuTrigger asChild>
+                        <Button
+                          aria-haspopup="true"
+                          size="icon"
+                          variant="ghost"
+                        >
+                          <MoreHorizontal className="h-4 w-4" />
+                          <span className="sr-only">Toggle menu</span>
+                        </Button>
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent align="end">
+                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                        <DropdownMenuItem>Edit</DropdownMenuItem>
+                        <DropdownMenuItem>Delete</DropdownMenuItem>
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell className="hidden sm:table-cell">
+                    <Image
+                      alt="Product image"
+                      className="aspect-square rounded-md object-cover"
+                      height="64"
+                      src="https://ui.shadcn.com/placeholder.svg"
+                      width="64"
+                    />
+                  </TableCell>
+                  <TableCell className="font-medium">
+                    Luminous VR Headset
+                  </TableCell>
+                  <TableCell>
+                    <Badge variant="outline">Active</Badge>
+                  </TableCell>
+                  <TableCell className="hidden md:table-cell">
+                    $199.99
+                  </TableCell>
+                  <TableCell className="hidden md:table-cell">
+                    30
+                  </TableCell>
+                  <TableCell className="hidden md:table-cell">
+                    2024-02-14 02:14 PM
+                  </TableCell>
+                  <TableCell>
+                    <DropdownMenu>
+                      <DropdownMenuTrigger asChild>
+                        <Button
+                          aria-haspopup="true"
+                          size="icon"
+                          variant="ghost"
+                        >
+                          <MoreHorizontal className="h-4 w-4" />
+                          <span className="sr-only">Toggle menu</span>
+                        </Button>
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent align="end">
+                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                        <DropdownMenuItem>Edit</DropdownMenuItem>
+                        <DropdownMenuItem>Delete</DropdownMenuItem>
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </CardContent>
+          <CardFooter>
+            <div className="text-xs text-muted-foreground">
+              Showing <strong>1-10</strong> of <strong>32</strong>{" "}
+              products
+            </div>
+          </CardFooter>
+        </Card>
+      </TabsContent>
+    </Tabs>
   </main>
 )
 
