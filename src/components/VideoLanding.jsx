deae3082a992390777ed002b917e0ff2f9966f40
kaandradec <130163890+kaandradec@users.noreply.github.com>
2024-06-27 01:52:26 -0500

    feat: boton Crear Evento

--- src/components/VideoLanding.jsx
+++ src/components/VideoLanding.jsx
@@ -1,24 +1,31 @@
-import React from "react";
-
 import "../styles/videoStyle.css";
-
-// const styles = {
-//   videoClipPath: {
-//     clipPath: "polygon(100% 0, 100% 85%, 50% 100%, 0 85%, 0 0)",
-//     "@media (max-width: 768px)": {
-//       clipPath: "polygon(100% 0, 100% 85%, 50% 90%, 0 85%, 0 0)",
-//     },
-//   },
-// };
+import { Button } from "@nextui-org/react";
+import { useNavigate } from "react-router-dom";
 
 export default function VideoLanding() {
+  const navigate = useNavigate();
+
   return (
     <div className="w-full relative">
-      <div className="w-full flex justify-center">
+      <section className="w-full flex justify-center">
         <h1 className="absolute z-10 text-2xl md:text-5xl text-white top-20 uppercase font-semibold max-w-7xl text-center text-balance px-1">
           Crea tu evento deseado y personalizado en minutos juento a Rent Event
         </h1>
-      </div>
+      </section>
+      <section className="flex justify-center items-center absolute z-50 inset-0 gap-4 max-w-xl mx-auto translate-y-96">
+        <h2 className="text-lg md:text-xl text-white font-semibold text-center px-1">
+          Empieza a crear tu evento y disfruta de una experiencia inolvidable
+        </h2>
+        <Button
+          onClick={() => navigate("/services")}
+          size="lg"
+          color="warning"
+          variant="shadow"
+          className="font-bold px-12"
+        >
+          CREAR EVENTO
+        </Button>
+      </section>
 
       <video
         className="w-full h-screen object-cover animate-fade animate-duration-1000 "
