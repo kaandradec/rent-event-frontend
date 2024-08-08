863ac594a46cc9f3fb33dfb85874a52614120a71
Kevin Andrade <130163890+kaandradec@users.noreply.github.com>
2024-08-07 22:57:28 -0500

    fix: update servicios

--- src/components/DashBoardServices/DashboardServicesNew.tsx
+++ src/components/DashBoardServices/DashboardServicesNew.tsx
@@ -64,9 +64,6 @@ export default function DashboardServicesNew({ code, setSubpage }: DashboardServ
         if (name === "" || description === "" || type === "" || state === "" || provider === "" || cost === 0) {
             setError("Campos vacíos");
             return false;
-        }else if(cost <= 50 || cost >= 100){
-            setError("Valor excedido");
-            return false;
         }
         return true;
     }
